91e21bd23d402ae261711c393e95cdd3725ee341
sireesha tammana <siri0981t@gmail.com>
2021-05-18 17:03:17 -0400

    cal

---
+++ src/calculator.java
@@ -0,0 +1,21 @@
+
+
+public class calculator{ 
+
+	public static void main(String[] args) {
+	
+	System.out.println(multiplication());
+		
+	}
+		public static int  multiplication() {
+		
+		 int a=4,  b=5,  m;
+		 
+		 m=a*b;
+		 
+		return m;
+	}	
+
+	}
+
+
